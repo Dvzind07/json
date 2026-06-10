cf7f6c2b7dc96d2a686c63ec933c843df5b7176c
Dv <rb.davi24@gmail.com>
2026-06-10 17:01:14 -0300

    Sistema de dados em json
    Update 10/06

--- sistema.js
+++ sistema.js
@@ -1,6 +1,20 @@
-const fs = require('fs')
+const fs = require('fs'); // Guarda o sistema de arquivos do JS
 
+const dados = fs.readFileSync('dados.json', 'utf-8'); // Lê o arquivo dados.json
 
+const usuarios = JSON.parse(dados); // Transforma o JSON em objeto JS
 
+usuarios.push({
+    id: 3,
+    nome: "Tarcisio",
+    idade: 40
+});
 
-const dados = fs.readFileSync('dados.json','utf-8')
+console.log("Usuários cadastrados no .json:", usuarios);
+
+fs.writeFileSync(
+    'dados.json',
+    JSON.stringify(usuarios, null, 2)
+);
+
+console.log("Usuários cadastrados");
