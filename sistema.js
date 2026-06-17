5e699004adf7d8fb9d01436c5b9a10882230632a
Dv <rb.davi24@gmail.com>
2026-06-17 17:06:06 -0300

    blajskj
    Update 17/06

--- sistema.js
+++ sistema.js
@@ -1,26 +1,21 @@
+const prompt = require("prompt-sync")();
+
 const fs = require('fs'); // Guarda o sistema de arquivos do JS
 
 const dados = fs.readFileSync('dados.json', 'utf-8'); // Lê o arquivo dados.json
 
 const usuarios = JSON.parse(dados); // Transforma o JSON em objeto JS
 
-usuarios.push({
-    id: 4,
-    nome: "Tarcisio",
-    idade: 40
-});
+const nome = prompt("Digite o nome do usuario:"); // Usuario digita o nome
+const idade = parseInt(prompt("Digite a idade do usuario:")); // Numero inteiro idade do usuario
 
-usuarios.push({
-    id: 5,
-    nome: "Murilo",
-    idade: 15
-});
 
-console.log("Usuários cadastrados no .json:", usuarios);
+const novoUsuario = {
+    id: usuarios.length + 1, // Id do usuario é o tamanho do array + 1
+    nome,
+    idade
+};
 
-fs.writeFileSync(
-    'dados.json',
-    JSON.stringify(usuarios, null, 2)
-);
-
-console.log("Usuários cadastrados");
+usuarios.push(novoUsuario); // Adiciona o novo usuario no array
+fs.writeFileSync("dados.json", JSON.stringify(usuarios, null, 2)); // Escreve o array atualizado no arquivo dados.json
+console.log("Usuario adicionado com sucesso!");
